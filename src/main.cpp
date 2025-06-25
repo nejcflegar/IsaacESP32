45e95a4446bb7c7ac7be120d1d18cd3523bfd607
Nejc Flegar <nejc.flegar.si@gmail.com>
2025-06-26 01:07:46 +0200

    yay

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <TFT_eSPI.h>
 #include <SPI.h>
+#include "OzadjeBase.h"
 
 TFT_eSPI tft = TFT_eSPI(); 
 TFT_eSprite sprit = TFT_eSprite(&tft);
 TFT_eSprite ozadje = TFT_eSprite(&tft);
+TFT_eSprite priloga = TFT_eSprite(&tft);
 
 int x = 0;
 int cas = 0;
@@ -11,7 +13,16 @@ void setup() {
   tft.init();  
   tft.setRotation(3);
   
-  //
+  ozadje.createSprite(238,240);
+  ozadje.setSwapBytes(1);
+  ozadje.pushImage(0,0,238,240,ozdajeBase);
+  ozadje.pushSprite(0,0);
+
+  priloga.createSprite(82,240);
+  priloga.fillSprite(TFT_BLACK);
+  priloga.pushSprite(238,0);
+
+  
 }
 
 void loop() {
