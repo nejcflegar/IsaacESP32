f74bd94e90afac39b2813adb8a0cef92bc41561d
Nejc Flegar <nejc.flegar.si@gmail.com>
2025-06-25 16:53:36 +0200

    First

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+TFT_eSPI tft = TFT_eSPI(); 
+TFT_eSprite sprit = TFT_eSprite(&tft);
+TFT_eSprite ozadje = TFT_eSprite(&tft);
+
+int x = 0;
+int cas = 0;
+void setup() {
+  tft.init();  
+  tft.setRotation(3);
+  
+  
+}
+
+void loop() {
+ 
+ 
+}
