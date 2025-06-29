7776d4b906d477555bd3bf6dab1d19b7022c25a9
Nejc Flegar <nejc.flegar.si@gmail.com>
2025-06-29 23:47:25 +0200

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,45 @@
 #include <TFT_eSPI.h>
-#include <SPI.h>
-#include "OzadjeBase.h"
+#include <Arduino.h>
+#include "ozadjeStuff.h"
+#include "ozadjeBase.h"
+#include "vrataDefault.h"
+#include "isaac.h"
+#include "body.h"
 
-TFT_eSPI tft = TFT_eSPI(); 
-TFT_eSprite sprit = TFT_eSprite(&tft);
+
+TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite ozadje = TFT_eSprite(&tft);
-TFT_eSprite priloga = TFT_eSprite(&tft);
+TFT_eSprite IsaacGlava = TFT_eSprite(&tft);
+TFT_eSprite IsaacBody = TFT_eSprite(&tft);
+TFT_eSprite vrata = TFT_eSprite(&tft);
 
-int x = 0;
-int cas = 0;
-void setup() {
-  tft.init();  
+void setup(){
+  tft.init();
   tft.setRotation(3);
-  
+
+  OzadjeSpechs* OzaST = new OzadjeSpechs();
   ozadje.createSprite(238,240);
-  ozadje.setSwapBytes(1);
-  ozadje.pushImage(0,0,238,240,ozdajeBase);
-  ozadje.pushSprite(0,0);
-
-  priloga.createSprite(82,240);
-  priloga.fillSprite(TFT_BLACK);
-  priloga.pushSprite(238,0);
-
+  ozadje.setSwapBytes(true);
+  ozadje.pushImage(0,0,238,240,ozadjeDefault);
   
+  IsaacBody.createSprite(32,32);
+  IsaacBody.setSwapBytes(false);
+  IsaacBody.pushImage(0,0,32,32,telo[0]);
+  IsaacBody.pushToSprite(&ozadje,100,100,TFT_WHITE);
+
+  IsaacGlava.createSprite(32,32);
+  IsaacGlava.setSwapBytes(false);
+  IsaacGlava.pushImage(0,0,32,32,glava[0]);
+  IsaacGlava.pushToSprite(&ozadje,100,80,TFT_WHITE);
+
+  vrata.createSprite(20,20);
+  vrata.setSwapBytes(false);
+  vrata.pushImage(0,0,20,20,vrataDefault[0]);
+  vrata.pushToSprite(&ozadje,100,0,TFT_WHITE);
+
+  ozadje.pushSprite(0,0);
 }
 
 void loop() {
- 
- 
+
 }
---
+++ src/ozadjeStuff.h
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+
+class OzadjeSpechs{
+    public: int sirina;
+    public: int visina;
+    public: int vrata[];
+
+
+    public: OzadjeSpechs(){
+        int sirina = 238;
+        int visina = 240;
+    }
+};
